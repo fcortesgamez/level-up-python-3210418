7a2ff8e0ac5c51523ca4567b9e7ddacb48b51274
Francisco Cortes Gamez <fcortesgamez@gmail.com>
2024-08-04 20:45:49 +0200

    rework sort words challenge

--- src/challengues/sortwords.py
+++ src/challengues/sortwords.py
@@ -1,7 +1,2 @@
-def sort_words(sentence):
-  words = sentence.split()
-  words = [w.lower() + w for w in words]
-  words.sort()
-  words = [w[len(w)//2:] for w in words]
-  return words
-
+def sort_words(words):
+    return " ".join(sorted(words.split(), key=str.casefold))
--- tests/test_sortwords.py
+++ tests/test_sortwords.py
@@ -4,14 +4,12 @@ from challengues.sortwords import sort_words
 
 class TestSortWords(unittest.TestCase):
     def test_sort_words(self):
-        self.assertEqual(sort_words("banana ORANGE apple"), ["apple", "banana", "ORANGE"])
-        self.assertEqual(sort_words("Hello world"), ["Hello", "world"])
-        self.assertEqual(sort_words("Python is awesome"), ["awesome", "is", "Python"])
-        self.assertEqual(
-            sort_words("Keep calm and code on"), ["and", "calm", "code", "Keep", "on"]
-        )
-        self.assertEqual(sort_words("This is a test"), ["a", "is", "test", "This"])
-        self.assertEqual(sort_words("Programming is fun"), ["fun", "is", "Programming"])
+        self.assertEqual(sort_words("banana ORANGE apple"), "apple banana ORANGE")
+        self.assertEqual(sort_words("Hello world"), "Hello world")
+        self.assertEqual(sort_words("Python is awesome"), "awesome is Python")
+        self.assertEqual(sort_words("Keep calm and code on"), "and calm code Keep on")
+        self.assertEqual(sort_words("This is a test"), "a is test This")
+        self.assertEqual(sort_words("Programming is fun"), "fun is Programming")
 
 
 if __name__ == "__main__":
